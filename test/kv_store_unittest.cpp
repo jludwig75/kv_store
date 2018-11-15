c98bb131b0c0931c46fa3151af9935fb4f69e6fa
Jonathan Ludwig <Jonathan.Ludwig@wdc.com>
2018-11-15 11:39:45 -0700

    Added lambdas for side effects

--- test/kv_store_unittest.cpp
+++ test/kv_store_unittest.cpp
@@ -19,26 +19,15 @@ extern "C" {
 
 struct test_value
 {
-	union
-	{
-		struct
-		{
-			size_t size;
-			char data[MAXBLOB];
-		};
-		struct value value;
-	};
-};
-
-
-class kv_open_Test : public testing::Test
-{
-    virtual void SetUp()
+    union
     {
-    }
-	virtual void TearDown()
-    {
-    }
+        struct
+        {
+            size_t size;
+            char data[MAXBLOB];
+        };
+        struct value value;
+    };
 };
 
 
@@ -79,112 +68,118 @@ MOCK_GLOBAL_FUNC4(kv_directory__lookup_key, int(struct kv_directory *directory, 
 MOCK_GLOBAL_FUNC2(kv_directory__remove_key, int(struct kv_directory *directory, uint64_t key));
 
 
-TEST_F(kv_open_Test, calls_block_array_open_with_file_name_and_create_flag)
+TEST(kv_open_Test, calls_block_array_open_with_file_name_and_create_flag)
 {
-	char *test_file_name = "test_file_name";
-	char *argv[] = { "dummy", test_file_name };
-	struct kvstor *store;
+    char *test_file_name = "test_file_name";
+    char *argv[] = { "dummy", test_file_name };
+    struct kvstor *store;
 
-	EXPECT_GLOBAL_CALL(kv_block_allocator__init, kv_block_allocator__init(testing::_, testing::_)).WillOnce(testing::Return(0));
-	EXPECT_GLOBAL_CALL(kv_block_array__init, kv_block_array__init(testing::_, testing::_)).WillOnce(testing::Return(0));
-	EXPECT_GLOBAL_CALL(kv_directory__init, kv_directory__init(testing::_)).WillOnce(testing::Return(0));
-	EXPECT_GLOBAL_CALL(kv_append_point__init, kv_append_point__init(testing::_, testing::_)).WillOnce(testing::Return(0));
-	EXPECT_GLOBAL_CALL(kv_block_array__open, kv_block_array__open(testing::_, test_file_name, true)).WillOnce(testing::Return(0));
+    EXPECT_GLOBAL_CALL(kv_block_allocator__init, kv_block_allocator__init(testing::_, testing::_)).WillOnce(testing::Return(0));
+    EXPECT_GLOBAL_CALL(kv_block_array__init, kv_block_array__init(testing::_, testing::_)).WillOnce(testing::Return(0));
+    EXPECT_GLOBAL_CALL(kv_directory__init, kv_directory__init(testing::_)).WillOnce(testing::Return(0));
+    EXPECT_GLOBAL_CALL(kv_append_point__init, kv_append_point__init(testing::_, testing::_)).WillOnce(testing::Return(0));
+    EXPECT_GLOBAL_CALL(kv_block_array__open, kv_block_array__open(testing::_, test_file_name, true)).WillOnce(testing::Return(0));
 
-	ASSERT_EQ(0, kv_open(&store, true, 2, argv));
+    ASSERT_EQ(0, kv_open(&store, true, 2, argv));
 
 
-	EXPECT_GLOBAL_CALL(kv_block_allocator__cleanup, kv_block_allocator__cleanup(testing::_));
-	EXPECT_GLOBAL_CALL(kv_block_array__cleanup, kv_block_array__cleanup(testing::_));
-	EXPECT_GLOBAL_CALL(kv_append_point__cleanup, kv_append_point__cleanup(testing::_));
-	EXPECT_GLOBAL_CALL(kv_directory__cleanup, kv_directory__cleanup(testing::_));
-	kv_close(store);
+    EXPECT_GLOBAL_CALL(kv_block_allocator__cleanup, kv_block_allocator__cleanup(testing::_));
+    EXPECT_GLOBAL_CALL(kv_block_array__cleanup, kv_block_array__cleanup(testing::_));
+    EXPECT_GLOBAL_CALL(kv_append_point__cleanup, kv_append_point__cleanup(testing::_));
+    EXPECT_GLOBAL_CALL(kv_directory__cleanup, kv_directory__cleanup(testing::_));
+    kv_close(store);
 }
 
-TEST_F(kv_open_Test, returns_failue_if_block_array_open_fails)
+TEST(kv_open_Test, returns_failue_if_block_array_open_fails)
 {
-	char *test_file_name = "test_file_name";
-	char *argv[] = { "dummy", test_file_name };
-	struct kvstor *store;
+    char *test_file_name = "test_file_name";
+    char *argv[] = { "dummy", test_file_name };
+    struct kvstor *store;
 
-	EXPECT_GLOBAL_CALL(kv_block_allocator__init, kv_block_allocator__init(testing::_, testing::_)).WillOnce(testing::Return(0));
-	EXPECT_GLOBAL_CALL(kv_block_array__init, kv_block_array__init(testing::_, testing::_)).WillOnce(testing::Return(0));
-	EXPECT_GLOBAL_CALL(kv_directory__init, kv_directory__init(testing::_)).WillOnce(testing::Return(0));
-	EXPECT_GLOBAL_CALL(kv_append_point__init, kv_append_point__init(testing::_, testing::_)).WillOnce(testing::Return(0));
-	EXPECT_GLOBAL_CALL(kv_block_array__open, kv_block_array__open(testing::_, test_file_name, false)).WillOnce(testing::Return(-77));
+    EXPECT_GLOBAL_CALL(kv_block_allocator__init, kv_block_allocator__init(testing::_, testing::_)).WillOnce(testing::Return(0));
+    EXPECT_GLOBAL_CALL(kv_block_array__init, kv_block_array__init(testing::_, testing::_)).WillOnce(testing::Return(0));
+    EXPECT_GLOBAL_CALL(kv_directory__init, kv_directory__init(testing::_)).WillOnce(testing::Return(0));
+    EXPECT_GLOBAL_CALL(kv_append_point__init, kv_append_point__init(testing::_, testing::_)).WillOnce(testing::Return(0));
+    EXPECT_GLOBAL_CALL(kv_block_array__open, kv_block_array__open(testing::_, test_file_name, false)).WillOnce(testing::Return(-77));
 
-	ASSERT_EQ(-77, kv_open(&store, false, 2, argv));
+    ASSERT_EQ(-77, kv_open(&store, false, 2, argv));
 
 
-	EXPECT_GLOBAL_CALL(kv_block_allocator__cleanup, kv_block_allocator__cleanup(testing::_));
-	EXPECT_GLOBAL_CALL(kv_block_array__cleanup, kv_block_array__cleanup(testing::_));
-	EXPECT_GLOBAL_CALL(kv_directory__cleanup, kv_directory__cleanup(testing::_));
-	EXPECT_GLOBAL_CALL(kv_append_point__cleanup, kv_append_point__cleanup(testing::_));
-	kv_close(store);
+    EXPECT_GLOBAL_CALL(kv_block_allocator__cleanup, kv_block_allocator__cleanup(testing::_));
+    EXPECT_GLOBAL_CALL(kv_block_array__cleanup, kv_block_array__cleanup(testing::_));
+    EXPECT_GLOBAL_CALL(kv_directory__cleanup, kv_directory__cleanup(testing::_));
+    EXPECT_GLOBAL_CALL(kv_append_point__cleanup, kv_append_point__cleanup(testing::_));
+    kv_close(store);
 }
 
-TEST_F(kv_open_Test, scans_the_log_for_existing_store)
+TEST(kv_open_Test, scans_the_log_for_existing_store)
 {
-	char *test_file_name = "test_file_name";
-	char *argv[] = { "dummy", test_file_name };
-	struct kvstor *store;
+    char *test_file_name = "test_file_name";
+    char *argv[] = { "dummy", test_file_name };
+    struct kvstor *store;
 
-	EXPECT_GLOBAL_CALL(kv_block_allocator__init, kv_block_allocator__init(testing::_, testing::_)).WillOnce(testing::Return(0));
-	EXPECT_GLOBAL_CALL(kv_block_array__init, kv_block_array__init(testing::_, testing::_)).WillOnce(testing::Return(0));
-	EXPECT_GLOBAL_CALL(kv_directory__init, kv_directory__init(testing::_)).WillOnce(testing::Return(0));
-	EXPECT_GLOBAL_CALL(kv_append_point__init, kv_append_point__init(testing::_, testing::_)).WillOnce(testing::Return(0));
-	EXPECT_GLOBAL_CALL(kv_block_array__open, kv_block_array__open(testing::_, test_file_name, false)).WillOnce(testing::Return(0));
+    EXPECT_GLOBAL_CALL(kv_block_allocator__init, kv_block_allocator__init(testing::_, testing::_)).WillOnce(testing::Return(0));
+    EXPECT_GLOBAL_CALL(kv_block_array__init, kv_block_array__init(testing::_, testing::_)).WillOnce(testing::Return(0));
+    EXPECT_GLOBAL_CALL(kv_directory__init, kv_directory__init(testing::_)).WillOnce(testing::Return(0));
+    EXPECT_GLOBAL_CALL(kv_append_point__init, kv_append_point__init(testing::_, testing::_)).WillOnce(testing::Return(0));
+    EXPECT_GLOBAL_CALL(kv_block_array__open, kv_block_array__open(testing::_, test_file_name, false)).WillOnce(testing::Return(0));
 
-	EXPECT_GLOBAL_CALL(kv_store__replay_log, kv_store__replay_log(testing::_)).WillOnce(testing::Return(0));
+    EXPECT_GLOBAL_CALL(kv_store__replay_log, kv_store__replay_log(testing::_)).WillOnce(testing::Return(0));
 
-	ASSERT_EQ(0, kv_open(&store, false, 2, argv));
+    ASSERT_EQ(0, kv_open(&store, false, 2, argv));
 
 
-	EXPECT_GLOBAL_CALL(kv_block_allocator__cleanup, kv_block_allocator__cleanup(testing::_));
-	EXPECT_GLOBAL_CALL(kv_block_array__cleanup, kv_block_array__cleanup(testing::_));
-	EXPECT_GLOBAL_CALL(kv_directory__cleanup, kv_directory__cleanup(testing::_));
-	EXPECT_GLOBAL_CALL(kv_append_point__cleanup, kv_append_point__cleanup(testing::_));
-	kv_close(store);
+    EXPECT_GLOBAL_CALL(kv_block_allocator__cleanup, kv_block_allocator__cleanup(testing::_));
+    EXPECT_GLOBAL_CALL(kv_block_array__cleanup, kv_block_array__cleanup(testing::_));
+    EXPECT_GLOBAL_CALL(kv_directory__cleanup, kv_directory__cleanup(testing::_));
+    EXPECT_GLOBAL_CALL(kv_append_point__cleanup, kv_append_point__cleanup(testing::_));
+    kv_close(store);
 }
 
 
-struct kvstor *store;
 class kv_set_Test : public testing::Test
 {
+protected:
     virtual void SetUp()
     {
-		char *test_file_name = "test_file_name";
-		char *argv[] = { "dummy", test_file_name };
+        char *test_file_name = "test_file_name";
+        char *argv[] = { "dummy", test_file_name };
+        
+        _block_allocator_ptr = (struct kv_block_allocator *)0x24534543;
+        _append_point_ptr = (struct kv_append_point *)0x42356;
+        EXPECT_GLOBAL_CALL(kv_block_allocator__init, kv_block_allocator__init(testing::_, testing::_)).WillOnce([this](struct kv_block_allocator **block_allocator, uint32_t number_of_blocks) { *block_allocator = _block_allocator_ptr; return 0; });
+        EXPECT_GLOBAL_CALL(kv_block_array__init, kv_block_array__init(testing::_, testing::_)).WillOnce(testing::Return(0));
+        EXPECT_GLOBAL_CALL(kv_directory__init, kv_directory__init(testing::_)).WillOnce(testing::Return(0));
+        EXPECT_GLOBAL_CALL(kv_append_point__init, kv_append_point__init(testing::_, testing::_)).WillOnce([this](struct kv_append_point **append_point, struct kv_block_allocator *block_allocator){ *append_point = _append_point_ptr; return 0;});
+        EXPECT_GLOBAL_CALL(kv_block_array__open, kv_block_array__open(testing::_, test_file_name, true)).WillOnce(testing::Return(0));
 
-		EXPECT_GLOBAL_CALL(kv_block_allocator__init, kv_block_allocator__init(testing::_, testing::_)).WillOnce(testing::Return(0));
-		EXPECT_GLOBAL_CALL(kv_block_array__init, kv_block_array__init(testing::_, testing::_)).WillOnce(testing::Return(0));
-		EXPECT_GLOBAL_CALL(kv_directory__init, kv_directory__init(testing::_)).WillOnce(testing::Return(0));
-		EXPECT_GLOBAL_CALL(kv_append_point__init, kv_append_point__init(testing::_, testing::_)).WillOnce(testing::Return(0));
-		EXPECT_GLOBAL_CALL(kv_block_array__open, kv_block_array__open(testing::_, test_file_name, true)).WillOnce(testing::Return(0));
-
-		ASSERT_EQ(0, kv_open(&store, true, 2, argv));
+        ASSERT_EQ(0, kv_open(&store, true, 2, argv));
     }
-	virtual void TearDown()
+    virtual void TearDown()
     {
-		EXPECT_GLOBAL_CALL(kv_block_allocator__cleanup, kv_block_allocator__cleanup(testing::_));
-		EXPECT_GLOBAL_CALL(kv_block_array__cleanup, kv_block_array__cleanup(testing::_));
-		EXPECT_GLOBAL_CALL(kv_directory__cleanup, kv_directory__cleanup(testing::_));
-		EXPECT_GLOBAL_CALL(kv_append_point__cleanup, kv_append_point__cleanup(testing::_));
-		kv_close(store);
+        EXPECT_GLOBAL_CALL(kv_block_allocator__cleanup, kv_block_allocator__cleanup(testing::_));
+        EXPECT_GLOBAL_CALL(kv_block_array__cleanup, kv_block_array__cleanup(testing::_));
+        EXPECT_GLOBAL_CALL(kv_directory__cleanup, kv_directory__cleanup(testing::_));
+        EXPECT_GLOBAL_CALL(kv_append_point__cleanup, kv_append_point__cleanup(testing::_));
+        kv_close(store);
     }
+    struct kvstor *store;
+    struct kv_block_allocator *_block_allocator_ptr;
+    struct kv_append_point *_append_point_ptr;
 };
 
 TEST_F(kv_set_Test, initializes_kv_block)
 {
-	struct test_value v1;
-	v1.size = 4;
+    struct test_value v1;
+    v1.size = 4;
 
-	struct key k;
-	k.id = 97;
+    struct key k;
+    k.id = 97;
 
-	EXPECT_GLOBAL_CALL(kv_block__init, kv_block__init(testing::_, k.id, v1.value.size, v1.value.data, 1));
+    EXPECT_GLOBAL_CALL(kv_block__init, kv_block__init(testing::_, k.id, v1.value.size, v1.value.data, 1));
 
-	EXPECT_GLOBAL_CALL(kv_append_point__get_append_point, kv_append_point__get_append_point(testing::_)).WillOnce(testing::Return(UINT32_MAX));
+    // Fail to allocate a block to exit kv_set here.
+    EXPECT_GLOBAL_CALL(kv_append_point__get_append_point, kv_append_point__get_append_point(_append_point_ptr)).WillOnce(testing::Return(UINT32_MAX));
 
-	ASSERT_EQ(-ENOSPC, kv_set(store, &k, &v1.value));
+    ASSERT_EQ(-ENOSPC, kv_set(store, &k, &v1.value));
 }
